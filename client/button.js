de8665747e89ef79dadbe6ea60d6d8861faecada
moss951 <ethanchmiel@gmail.com>
2025-07-13 14:30:38 -0400

    main menu and lobby screens and buttons

---
+++ client/button.js
@@ -0,0 +1,38 @@
+class Button {
+    constructor(x, y, w, h, text) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.text = text;
+        this.clicked = false;
+    }
+
+    draw() {
+        if (this.clicked) {
+            ctx.fillStyle = "lightgray";
+            ctx.beginPath();
+            ctx.rect(this.x, this.y, this.w, this.h);
+            ctx.fill();
+        }
+
+        ctx.strokeStyle = "black";
+        for (let i = 0; i < 3; i++) {
+            ctx.beginPath();
+            ctx.rect(this.x, this.y, this.w, this.h);
+            ctx.stroke();
+        }
+
+        ctx.fillStyle = "black";
+        ctx.font = "48px DS-BIOS";
+        ctx.fillText(this.text, this.x + this.w / 2 - ctx.measureText(this.text).width / 2, this.y + 38);
+    }
+
+    isClicked(x, y) {
+        if (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.h) {
+            this.clicked = true;
+            return true;
+        }
+        return false;
+    }
+}
--- client/client.js
+++ client/client.js
@@ -1,4 +1,5 @@
 const canvas = document.getElementById("game");
+const bounds = canvas.getBoundingClientRect();
 const ctx = canvas.getContext("2d");
 const socket = io();
 
@@ -22,161 +23,113 @@ canvas.width = canvas.clientWidth / scaleFactor;
 canvas.height = canvas.clientHeight / scaleFactor;
 ctx.scale(1 / scaleFactor, 1 / scaleFactor);
 
-document.getElementById("play").addEventListener("click", () => {
-    document.getElementById("play").disabled = true;
-    let username = document.getElementById("username").value;
-    socket.emit("join", { username });
-    joinLobby();
-});
+// main menu stuff
+let cursorInputTimer = 0;
+let username = "";
+let playButton = new Button(300, 400, 200, 50, "play");
 
-function joinLobby() {
-    document.getElementById("ready").addEventListener("click", () => {
-        document.getElementById("ready").disabled = true;
+// lobby stuff
+let readyButton = new Button(300, 400, 200, 50, "ready");
+
+initMainMenu();
+
+function initMainMenu() {
+    drawIntervalId = setInterval(drawMainMenu, 1000 / 60);
+    document.addEventListener("keydown", typeUsername);
+    document.addEventListener("mousedown", mainMenuMouseDown);
+    document.addEventListener("mouseup", mainMenuMouseUp);
+}
+
+function typeUsername(e) {
+    if (e.key == "Backspace") {
+        username = username.slice(0, username.length - 1);
+    }
+    else {
+        if (e.key.length == 1) username += e.key;
+    }
+}
+
+function mainMenuMouseDown(e) {
+    if (playButton.isClicked(e.clientX - bounds.left, e.clientY - bounds.top)) {}
+}
+
+function mainMenuMouseUp() {
+    playButton.clicked = false;
+
+    if (username.length > 0) {
+        socket.emit("join", { username });
+        document.removeEventListener("keydown", typeUsername);
+        document.removeEventListener("mousedown", mainMenuMouseDown);
+        document.removeEventListener("mouseup", mainMenuMouseUp);
+        clearInterval(drawIntervalId);
+        initLobby();
+    }
+}
+
+function drawMainMenu() {
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    drawGrid();
+
+    ctx.font ="128px DS-BIOS";
+    ctx.fillStyle = "black";
+    ctx.fillText("EVIL SHAPE", canvas.clientWidth / 2 - ctx.measureText("EVIL SHAPE").width / 2, canvas.clientHeight / 3);
+    playButton.draw();
+
+    // username input
+    for (let i = 0; i < 3; i++) {
+        ctx.beginPath();
+        ctx.rect(150, 300, 500, 50);
+        ctx.stroke();
+    }
+
+    cursorInputTimer++;
+    if (cursorInputTimer >= 24) {
+        ctx.beginPath();
+        ctx.rect(150 + 5 + ctx.measureText(username).width, 300 + 5, 2, 50 - 10);
+        ctx.fill();
+    }
+    if (cursorInputTimer >= 48) cursorInputTimer = 0;
+    ctx.fillText(username, 150 + 5, 300 + 38);
+}
+
+// lobby stuff
+
+function initLobby() {
+    drawIntervalId = setInterval(drawLobby, 1000 / 60);
+
+    document.addEventListener("mousedown", lobbyMouseDown);
+    document.addEventListener("mouseup", lobbyMouseUp);
+}
+
+function lobbyMouseDown(e) {
+    if (readyButton.isClicked(e.clientX - bounds.left, e.clientY - bounds.top)) {
         ready = true;
         socket.emit("ready");
-    });
+    }
 }
 
-function draw() {
-    ctx.save();
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
+function lobbyMouseUp() {
+    readyButton.clicked = false;
+}
+
+function drawLobby() {
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-    ctx.restore();
-
     drawGrid();
-    level.draw();
 
-    for (let i = 0; i < particles.length / PARTICLES_ALONG_EDGE; i++) {
-        particles[i * PARTICLES_ALONG_EDGE].draw(usernames[i]);
-    }
+    ctx.fillStyle = "black";
+    ctx.font = "64px DS-BIOS";
+    ctx.fillText("Players", 50, ctx.measureText("Players").actualBoundingBoxAscent + ctx.measureText("Players").actualBoundingBoxDescent + 50);
 
-    for (let i = 0; i < cursorParticles.length; i++) {
-        cursorParticles[i].draw(usernames[i], particles[i * PARTICLES_ALONG_EDGE].currentBoostTime);
-    }
-
-    for (let i = 0; i < springs.length; i++) {
-        springs[i].draw();
-    }
-
-    // scroll
-    let previousScrollOffset = { ...scrollOffset };
-    updateScollOffset()
-    socket.emit("cursorMoveRequest", { x:scrollOffset.x - previousScrollOffset.x, y:scrollOffset.y - previousScrollOffset.y});
-}
-
-socket.on('updatePlayerList', (serverPlayers) => {
-    let playersFound = {};
-
-    for (let id in serverPlayers) {
-        if (players[id] == undefined) {
-            players[id] = new PlayerClient(serverPlayers[id].username, serverPlayers[id].ready);
-        }
-        
-        playersFound[id] = true;
-    }
-
+    ctx.font = "48px DS-BIOS";
+    let i = 0;
     for (let id in players) {
-        if (!playersFound[id]) delete players[id];
-    }
-});
-
-socket.on('initGame', (gameData) => {
-    for (let id in gameData.players) {
-        players[id].particleIndex = gameData.players[id].particleIndex;
-        players[id].cursorParticleIndex = gameData.players[id].cursorParticleIndex;
+        ctx.fillStyle = "black";
+        ctx.fillText(players[id].username, 50, ctx.measureText(players[id].username).actualBoundingBoxAscent + ctx.measureText(players[id].username).actualBoundingBoxDescent + 48 * i + 64 + 50);
+        i++;
     }
 
-    // make particles
-    for (let i = 0; i < gameData.particles.length; i++) {
-        particles.push(new ParticleClient(gameData.particles[i].x, gameData.particles[i].y, gameData.particles[i].isPlayer));
-    }
-
-    for (let i = 0; i < gameData.cursorParticles.length; i++) {
-        cursorParticles.push(new CursorParticleClient(gameData.cursorParticles[i].x, gameData.cursorParticles[i].y, gameData.cursorParticles[i].attractionRadius));
-    }
-
-    playerParticle = particles[players[socket.id].particleIndex];
-    playerCursorParticle = cursorParticles[players[socket.id].cursorParticleIndex];
-
-    // make springs
-    for (let i = 0; i < gameData.springs.length; i++) {
-        let particle1, particle2;
-        for (let j = 0; j < particles.length; j++) {
-            if (gameData.springs[i].particle1.x == particles[j].x && gameData.springs[i].particle1.y == particles[j].y) {
-                particle1 = particles[j];
-            }
-
-            if (gameData.springs[i].particle2.x == particles[j].x && gameData.springs[i].particle2.y == particles[j].y) {
-                particle2 = particles[j];
-            }
-        }
-        springs.push(new SpringClient(particle1, particle2));
-    }
-
-    // load level
-    for (let i = 0; i < gameData.walls.length; i++) {
-        level.walls.push(new RectClient(gameData.walls[i].x, gameData.walls[i].y, gameData.walls[i].w, gameData.walls[i].h));
-    }
-
-    // get usernames from dictionary in array
-    for (let i = 0; i < particles.length; i++) {
-        for (let id in players) {
-            if (i == players[id].particleIndex) {
-                usernames.push(players[id].username);
-                continue;
-            }
-        }
-    }
-
-    socket.emit("cursorResetPosRequest", { x:playerParticle.x, y:playerParticle.y });
-    canvas.requestPointerLock();
-    drawIntervalId = setInterval(draw, 1000 / 60);
-
-    document.addEventListener("mousemove", (e) => {
-        if (document.pointerLockElement == canvas) {
-            socket.emit("cursorMoveRequest", { x:e.movementX, y:e.movementY });
-        }
-    });
-
-    document.addEventListener("mousedown", (e) => {
-        if (document.pointerLockElement != canvas) {
-            socket.emit("cursorResetPosRequest", { x:e.clientX + scrollOffset.x, y:e.clientY + scrollOffset.y });
-            canvas.requestPointerLock();
-        }
-
-        socket.emit("cursorBoostStartRequest");
-    });
-
-    document.addEventListener("mouseup", () => {
-        socket.emit("cursorBoostStopRequest");
-    });
-
-    document.addEventListener("focus", () => {
-        updateScollOffset();
-    });
-
-    // debug controls
-    document.addEventListener("keydown", (e) => {
-        if (e.key == "s") ctx.translate(0, -25);
-        if (e.key == "w") ctx.translate(0, 25);
-        if (e.key == "d") ctx.translate(-25, 0);
-        if (e.key == "a") ctx.translate(25, 0);
-    });
-});
-
-socket.on('updateGame', (gameData) => {
-    // update particles
-    for (let i = 0; i < particles.length; i++) {
-        particles[i].x = gameData.particles[i].x;
-        particles[i].y = gameData.particles[i].y;
-        particles[i].currentBoostTime = gameData.particles[i].currentBoostTime;
-    }
-
-    for (let i = 0; i < cursorParticles.length; i++) {
-        cursorParticles[i].x = gameData.cursorParticles[i].x;
-        cursorParticles[i].y = gameData.cursorParticles[i].y;
-    }
-});
+    readyButton.draw();
+}
 
 function updateScollOffset() {
     if (playerParticle.x > canvas.clientWidth / 2 + scrollOffset.x) {
@@ -235,3 +188,151 @@ function drawGrid() {
 
     ctx.restore();
 }
+
+function drawGame() {
+    ctx.save();
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    ctx.restore();
+
+    drawGrid();
+    level.draw();
+
+    for (let i = 0; i < particles.length / PARTICLES_ALONG_EDGE; i++) {
+        particles[i * PARTICLES_ALONG_EDGE].draw(usernames[i]);
+    }
+
+    for (let i = 0; i < cursorParticles.length; i++) {
+        cursorParticles[i].draw(usernames[i], particles[i * PARTICLES_ALONG_EDGE].currentBoostTime);
+    }
+
+    for (let i = 0; i < springs.length; i++) {
+        springs[i].draw();
+    }
+
+    // scroll
+    let previousScrollOffset = { ...scrollOffset };
+    updateScollOffset()
+    socket.emit("cursorMoveRequest", { x:scrollOffset.x - previousScrollOffset.x, y:scrollOffset.y - previousScrollOffset.y});
+}
+
+socket.on('updatePlayerList', (serverPlayers) => {
+    let playersFound = {};
+
+    for (let id in serverPlayers) {
+        if (players[id] == undefined) {
+            players[id] = new PlayerClient(serverPlayers[id].username, serverPlayers[id].ready);
+        }
+        
+        playersFound[id] = true;
+    }
+
+    for (let id in players) {
+        if (!playersFound[id]) delete players[id];
+    }
+});
+
+socket.on('initGame', (gameData) => {
+    clearInterval(drawIntervalId);
+    document.removeEventListener("mousedown", lobbyMouseDown);
+    document.removeEventListener("mouseup", lobbyMouseUp);
+
+    for (let id in gameData.players) {
+        players[id].particleIndex = gameData.players[id].particleIndex;
+        players[id].cursorParticleIndex = gameData.players[id].cursorParticleIndex;
+    }
+
+    // make particles
+    for (let i = 0; i < gameData.particles.length; i++) {
+        particles.push(new ParticleClient(gameData.particles[i].x, gameData.particles[i].y, gameData.particles[i].isPlayer));
+    }
+
+    for (let i = 0; i < gameData.cursorParticles.length; i++) {
+        cursorParticles.push(new CursorParticleClient(gameData.cursorParticles[i].x, gameData.cursorParticles[i].y, gameData.cursorParticles[i].attractionRadius));
+    }
+
+    playerParticle = particles[players[socket.id].particleIndex];
+    playerCursorParticle = cursorParticles[players[socket.id].cursorParticleIndex];
+
+    // make springs
+    for (let i = 0; i < gameData.springs.length; i++) {
+        let particle1, particle2;
+        for (let j = 0; j < particles.length; j++) {
+            if (gameData.springs[i].particle1.x == particles[j].x && gameData.springs[i].particle1.y == particles[j].y) {
+                particle1 = particles[j];
+            }
+
+            if (gameData.springs[i].particle2.x == particles[j].x && gameData.springs[i].particle2.y == particles[j].y) {
+                particle2 = particles[j];
+            }
+        }
+        springs.push(new SpringClient(particle1, particle2));
+    }
+
+    // load level
+    for (let i = 0; i < gameData.walls.length; i++) {
+        level.walls.push(new RectClient(gameData.walls[i].x, gameData.walls[i].y, gameData.walls[i].w, gameData.walls[i].h));
+    }
+
+    // get usernames from dictionary in array
+    for (let i = 0; i < particles.length; i++) {
+        for (let id in players) {
+            if (i == players[id].particleIndex) {
+                usernames.push(players[id].username);
+                continue;
+            }
+        }
+    }
+
+    // init cursor pos
+    socket.emit("cursorResetPosRequest", { x:playerParticle.x, y:playerParticle.y });
+    canvas.requestPointerLock();
+
+    // start game and inputs
+    drawIntervalId = setInterval(drawGame, 1000 / 60);
+
+    document.addEventListener("mousemove", (e) => {
+        if (document.pointerLockElement == canvas) {
+            socket.emit("cursorMoveRequest", { x:e.movementX, y:e.movementY });
+        }
+    });
+
+    document.addEventListener("mousedown", (e) => {
+        if (document.pointerLockElement != canvas) {
+            socket.emit("cursorResetPosRequest", { x:e.clientX - bounds.left + scrollOffset.x, y:e.clientY - bounds.top + scrollOffset.y });
+            canvas.requestPointerLock();
+        }
+
+        socket.emit("cursorBoostStartRequest");
+    });
+
+    document.addEventListener("mouseup", () => {
+        socket.emit("cursorBoostStopRequest");
+    });
+
+    document.addEventListener("focus", () => {
+        updateScollOffset();
+    });
+
+    // debug controls
+    document.addEventListener("keydown", (e) => {
+        if (e.key == "s") ctx.translate(0, -25);
+        if (e.key == "w") ctx.translate(0, 25);
+        if (e.key == "d") ctx.translate(-25, 0);
+        if (e.key == "a") ctx.translate(25, 0);
+    });
+});
+
+socket.on('updateGame', (gameData) => {
+    // update particles
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].x = gameData.particles[i].x;
+        particles[i].y = gameData.particles[i].y;
+        particles[i].currentBoostTime = gameData.particles[i].currentBoostTime;
+    }
+
+    for (let i = 0; i < cursorParticles.length; i++) {
+        cursorParticles[i].x = gameData.cursorParticles[i].x;
+        cursorParticles[i].y = gameData.cursorParticles[i].y;
+    }
+});
